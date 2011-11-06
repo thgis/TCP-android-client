9a5e511107b7c5f7230602982793d2d0bd2218d9
unknown <LG@M6300LK.DanRob.local>
2011-11-06 22:37:31 +0100

    The begining of a new superchat program!

---
+++ src/iglugis/chatter/ChatterActivity.java
@@ -0,0 +1,69 @@
+package iglugis.chatter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.EditText;
+
+public class ChatterActivity extends Activity {
+	private String ipAddress = "192.168.16.200";
+	private Client client;
+	
+	private EditText textToSend;
+	private EditText textReceived;
+	private EditText textUser;
+	private EditText serverIP;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        setContentView(R.layout.main);
+        
+        textToSend = (EditText)findViewById(R.id.ETSend);
+        textReceived = (EditText)findViewById(R.id.ETReceived);
+        textUser = (EditText)findViewById(R.id.ETUserName);
+        serverIP = (EditText)findViewById(R.id.ETServerIP);
+        
+        serverIP.setText(ipAddress);
+        textUser.setText("LarsMobi");
+    }
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    }
+    
+    @Override
+    protected void onStop() {
+    	if(client != null)
+    		client.Stop();
+    	super.onStop();
+    }
+    
+    private Handler handlerClient = new Handler() {
+   	 
+        public void handleMessage(Message msg) {
+          if (msg.what == 0) {
+            textReceived.append((String)msg.obj + "\n" );
+          }
+        }
+      };
+    
+    public void Connect(View view)
+    {
+    	ipAddress = serverIP.getText().toString();
+    	client = new Client(ipAddress,textUser.getText().toString(),handlerClient);
+    	client.Start();
+    }
+    
+    public void Send(View view)
+    {
+    	String msg = textToSend.getText().toString();
+    	client.SendMessage(msg);
+    	textToSend.setText("");
+    }
+}
---
+++ src/iglugis/chatter/Client.java
@@ -0,0 +1,112 @@
+package iglugis.chatter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class Client implements Runnable {
+	private Socket kkSocket;
+	private byte[] buffer;
+	private String Name;
+	private Handler handle;
+	
+	OutputStream outStream;
+	InputStream instream;
+	
+	private volatile boolean stop = false;
+	
+	public Client(String ipAddress, String name, Handler handle)
+	{
+		this.handle = handle;
+		Name = name;
+		buffer = new byte[1024];
+		try {
+			kkSocket = new Socket(ipAddress , 8000);
+			outStream = kkSocket.getOutputStream();
+			instream = kkSocket.getInputStream();
+			SendMessage("User:" + Name);
+		} catch (UnknownHostException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+    public void Read()
+    {
+		try {
+			
+	        int bytes = instream.available();
+	        if( bytes > 0 )
+	        {
+	        	 instream.read(buffer, 0, bytes);
+	        	 String test = new String(buffer,0,bytes);
+	 	         
+	        	 Message lmsg = new Message();
+	 	         lmsg.what = 0;
+	 	         lmsg.obj = test;
+	 	         handle.sendMessage(lmsg);
+	        }
+	        
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    public void SendMessage(String msg)
+    {
+        byte[] msgByte = msg.getBytes();
+        
+        try {
+			outStream.write(msgByte, 0, msgByte.length);
+			outStream.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    public synchronized void Start()
+    {
+    	Thread listenThread = new Thread(this);
+    	listenThread.start();
+    }
+
+	@Override
+	public void run() {
+		while(!stop)
+		{
+			Read();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		Shutdown();
+	}
+	
+	private void Shutdown()
+	{
+    	try {
+			kkSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	kkSocket = null;
+	}
+    
+    public synchronized void Stop()
+    {
+    	stop = true;
+    }
+}
